4a4bf85ba5181366ac6e44f641a9530b56a7b321
Francis <kikobilas123@gmail.com>
2025-07-26 01:30:23 +0800

    s

--- client/src/components/CreateChat.tsx
+++ client/src/components/CreateChat.tsx
@@ -1,23 +1,15 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  type ChangeEvent,
-  type FormEvent,
-} from "react";
+import { useEffect, useState, type ChangeEvent, type FormEvent } from "react";
 import Plus from "../assets/plus.svg";
 import { useAppDispatch } from "../redux/Hooks";
 import { CreateRoomThunk } from "../redux/thunks/roomThunk";
 
 const CreateChat = () => {
   const dispatch = useAppDispatch();
-  const modalRef = useRef<HTMLDialogElement>(null);
-
   const [tagsData, setTagsData] = useState<string[]>([]);
   const [roomData, setRoomData] = useState({
     name: "",
     description: "",
-    tags: [] as string[],
+    tags: tagsData,
   });
 
   const handleChange = (
@@ -25,26 +17,29 @@ const CreateChat = () => {
   ) => {
     const { name, value } = e.target;
 
-    setRoomData((prev) => ({
-      ...prev,
+    setRoomData({
+      ...roomData,
       [name]: value,
-    }));
-  };
-
-  const handleTagChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { checked, value } = e.target;
-
-    setTagsData((prev) =>
-      checked ? [...prev, value] : prev.filter((tag) => tag !== value)
-    );
+    });
   };
 
   useEffect(() => {
-    setRoomData((prev) => ({
-      ...prev,
+    setRoomData({
+      ...roomData,
       tags: tagsData,
-    }));
+    });
   }, [tagsData]);
+
+  // Changes for tags
+  const handleTagChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { checked, value } = e.target;
+
+    if (checked) {
+      setTagsData((prev) => [...prev, value]);
+    } else {
+      setTagsData((prev) => prev.filter((tag) => tag !== value));
+    }
+  };
 
   const handleCreateRoom = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -52,80 +47,84 @@ const CreateChat = () => {
     const result = await dispatch(CreateRoomThunk(roomData));
 
     if (CreateRoomThunk.fulfilled.match(result)) {
-      // Reset form
       setTagsData([]);
-      setRoomData({ name: "", description: "", tags: [] });
-
-      // Close modal
-      modalRef.current?.close();
+      setRoomData({
+        name: "",
+        description: "",
+        tags: tagsData,
+      });
     }
   };
 
   return (
     <>
       <div
-        onClick={() => modalRef.current?.showModal()}
+        onClick={() => {
+          const modal = document.getElementById(
+            "my_modal_3"
+          ) as HTMLDialogElement | null;
+          modal?.showModal();
+        }}
         className="w-full shadow-lg p-2 grid place-items-center cursor-pointer rounded-lg hover:bg-gray-300"
       >
-        <img src={Plus} alt="Add" className="aspect-square w-10" />
+        <img src={Plus} alt="" className="aspect-square w-10" />
       </div>
-
-      <dialog ref={modalRef} id="my_modal_3" className="modal">
+      <dialog id="my_modal_3" className="modal">
         <div className="modal-box">
-          <form method="dialog" className="mb-4">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
               ✕
             </button>
           </form>
-          <h3 className="font-bold text-lg mb-4">Create a New Party Chat</h3>
-          <form onSubmit={handleCreateRoom} className="flex flex-col gap-4">
-            <fieldset className="w-full">
-              <legend className="font-semibold text-sm mb-1">Party Name</legend>
+          <h3 className="font-bold text-lg">Create a New Party Chat</h3>
+          <form
+            onSubmit={handleCreateRoom}
+            className="w-full flex flex-col gap-2"
+          >
+            <fieldset className="fieldset w-full">
+              <legend className="fieldset-legend">Party Name</legend>
               <input
                 type="text"
                 name="name"
-                className="input input-bordered w-full"
+                className="input w-full"
                 placeholder="Type here"
                 value={roomData.name}
                 onChange={handleChange}
-                required
               />
             </fieldset>
-
-            <fieldset>
-              <legend className="font-semibold text-sm mb-1">
-                Description
-              </legend>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Description</legend>
               <textarea
                 name="description"
-                className="textarea textarea-bordered h-24 w-full"
+                id=""
+                className="textarea h-24 w-full"
                 rows={6}
                 value={roomData.description}
                 onChange={handleChange}
-                required
-              />
+              ></textarea>
             </fieldset>
 
-            <fieldset>
-              <legend className="font-semibold text-sm mb-1">Tags</legend>
-              <div className="flex items-center gap-2">
-                <input
-                  type="checkbox"
-                  id="chat"
-                  value="chat"
-                  onChange={handleTagChange}
-                  checked={tagsData.includes("chat")}
-                  className="checkbox checkbox-neutral"
-                />
-                <label htmlFor="chat" className="text-lg">
-                  Chat
-                </label>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Tags</legend>
+
+              <div>
+                <div className="flex items-center gap-2 cursor-pointer">
+                  <input
+                    type="checkbox"
+                    id="chat"
+                    value="chat"
+                    onChange={handleTagChange}
+                    checked={tagsData.includes("chat")}
+                    className="checkbox checkbox-neutral"
+                  />
+                  <label htmlFor="chat" className="text-lg">
+                    Chat
+                  </label>
+                </div>
               </div>
             </fieldset>
-
-            <button className="btn btn-outline btn-success mt-2" type="submit">
-              Create
-            </button>
+            <button className="btn btn-outline btn-success">Create</button>
           </form>
         </div>
       </dialog>
