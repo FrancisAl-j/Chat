9adc5ced178646b76841149f69742ceb058cea35
Francis <kikobilas123@gmail.com>
2025-07-25 23:36:53 +0800

    Trying to fix the setting cookies

--- server/utils/generateToken.ts
+++ server/utils/generateToken.ts
@@ -11,7 +11,7 @@ export const generateToken = async (payload: Payload, res: Response) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "lax",
-    secure: false,
+    secure: true,
   });
 
   return token;
