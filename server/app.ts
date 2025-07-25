9c76b7dcd5ba610c35d5f07faa24d86f63f31afe
Francis <kikobilas123@gmail.com>
2025-07-25 22:47:52 +0800

    Change: remove the old url of localhost

--- server/app.ts
+++ server/app.ts
@@ -13,7 +13,7 @@ import { app, server } from "./utils/socket";
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chatglobal-socket.vercel.app/",
     credentials: true,
   })
 );
